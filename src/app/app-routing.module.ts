3f01abb139c8c1d84307e86f251790fb11cca375
MDG369 <michazieli@gmail.com>
2024-06-30 23:48:55 +0200

    docker deployment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import {RouterModule, Routes} from '@angular/router';
 import {NgModule} from "@angular/core";
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', redirectTo: '', pathMatch: 'full' }];
 @NgModule({
   imports: [RouterModule.forRoot(routes, {onSameUrlNavigation: 'reload'})],
   exports: [RouterModule]
